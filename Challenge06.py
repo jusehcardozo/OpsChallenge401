59de3118bd85bac5d31de49ada8f861fe8517f09
jusehcardozo <joseccardozo93@gmail.com>
2023-01-24 14:59:53 -0800

    This works

--- Challenge06.py
+++ Challenge06.py
@@ -8,12 +8,14 @@
 # Import Libraries
 
 from cryptography.fernet import Fernet
+from os.path import exists
 
 # Function that generates a key
 def write_key():
     # Generates a key and save it into a file
     
     key = Fernet.generate_key()
+    # Creates file key.key and saves the value into a file
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
@@ -21,44 +23,77 @@ def write_key():
 # Function that loads the generated key
 def load_key():
     # Loads the key from the current directory named `key.key`
-    
+    # Read method reads the content
     return open("key.key", "rb").read()
 
 
 
 # Function that encrypts a message
 def encrypt_message():
-    encrypted = f.encrypt(message)
+    user_message = input("What message would you like to Encrypt?")
+    enconded_message = user_message.encode()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(enconded_message)
+    print("This is the encrypted Message")
+    # encrypted = f.encrypt(message)
     # print how it looks
-    print(encrypted)
+    print(encrypted_message)
 
 # Function that decrypts a message
 def decrypt_message():
-    decrypted_encrypted = f.decrypt(encrypted)
+    user_message = input("What message would you like to encrypt")
+    # Decodes the encrypted message provided by the user
+    decoded_message = str.encoded(user_message)
+    # Initialize Fernet class
+    f = Fernet(key)
+    # Decrypt the message
+    decrypted_encrypted = f.decrypt(decoded_message)
+    print("This is you decrypted message:")
     print(decrypted_encrypted)
 
 
 # Function that encrypts a file
-def encrypt_file(filename, key):
-    # Given a filename (str) and key (bytes), it encrypts the file and write it
+def encrypt_file():
+    # Asking the full file path to be encrypted
+    file_name = input("Enter the file path to be encrypted")
+    # This opens the file and reads the contents
+    with open(file_name, "rb") as file:
+        # This reads the contents of the file and saves it in a variable called file_data
+        file_data = file.read()
+
+    # Initialize the Fernet class
     f = Fernet(key)
+
+    # Encrypt the contents of the file and save in a variable called encrypted_file
+    encrypted_file = f.encrypt(file_data)
+
+    # Write the encrypted data to a file
+    with open(file_name, "wb") as file:
+        file.write(encrypted_file)
 
 
 # Function that decrypts a file
-def decrypt_file(filename, key):
+def decrypt_file():
 
-    # Given a filename (str) and key (bytes), it decrypts the file and write it
+    # Ask the path to be decrypted
+    file_name = input("Enter the file path to be decrypted")
+
+    # This opens the file and reads the contents
+    with open(file_name, "rb") as file:
+        # This reads the contents of the file and saves it in a variable called file_data
+        file_data = file.read()
+
+        # Initialize the Fernet class
+        f = Fernet(key)
+
+        # Decrypt data
+        decrypted_data = f.decrypt(file_data)
+
+        # Write the decrypted data to a file
+    with open(file_name, "wb") as file:
+        file.write(decrypted_file)
+    
    
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        # read the encrypted data
-        encrypted_data = file.read()
-    # decrypt data
-    decrypted_data = f.decrypt(encrypted_data)
-    # write the original file
-    with open(filename, "wb") as file:
-        file.write(decrypted_data)
-
 
 # Function that gives a user a menu
 def ask_user():
@@ -79,10 +114,16 @@ def ask_user():
     elif ((mode != "1") and (mode != "2") and (mode != "3") and (mode != "4")):
         print("Wrong Choice! Choose again.")
     
+key_exists = exists('./key.key')
 
-
+# Check if the key file exists then call the load_key funtion to read it
+if key_exists:
+    key = load_key()
+else:
+    write_key()
+    key = load_key()
 
 # Infinite loop for user menu
 # USE CONTROL C TO EXIT LOOP!!!
 while True:
-    ask_user()
+     ask_user()
