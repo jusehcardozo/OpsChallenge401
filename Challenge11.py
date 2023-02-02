38d430d59db61f332a755f0afbca761a37aae80e
jusehcardozo <joseccardozo93@gmail.com>
2023-02-01 21:00:47 -0800

    Needs work

--- Challenge11.py
+++ Challenge11.py
@@ -25,15 +25,15 @@ for dstPort in portRange:
 	srcPort = random.randint(1025,65534)
 	resp = sr1(IP(dst=host)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
 	if (str(type(resp)) == "<type 'NoneType'>"):
-		print host + ":" + str(dstPort) + " is filtered (silently dropped)."
-	elif(resp.haslayer(TCP)):
+		print(host + ":" + str(dstPort) + " is filtered (silently dropped).")
+	# elif(resp.haslayer(TCP)):
 	
     if(resp.getlayer(TCP).flags == 0x12):
-	send_rst = sr(IP(dst=host)/TCP(sport=srcPort,dport=dstPort,flags="R"),timeout=1,verbose=0)
-	print host + ":" + str(dstPort) + " is open."
+		send_rst = sr(IP(dst=host)/TCP(sport=srcPort,dport=dstPort,flags="R"),timeout=1,verbose=0)
+		print(host + ":" + str(dstPort) + " is open.")
 	elif (resp.getlayer(TCP).flags == 0x14):
-	print host + ":" + str(dstPort) + " is closed."
-	elif(resp.haslayer(ICMP)):
+		print(host + ":" + str(dstPort) + " is closed.")
+	# elif(resp.haslayer(ICMP)):
 	if(int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-	print host + ":" + str(dstPort) + " is filtered (silently dropped)."
+		print host + ":" + str(dstPort) + " is filtered (silently dropped)."
 
