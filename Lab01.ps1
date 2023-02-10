328b6e13296713892fc20b10ed06cbc290fa1771
jusehcardozo <joseccardozo93@gmail.com>
2023-02-09 16:17:31 -0800

    Finally done

---
+++ Lab01.ps1
@@ -0,0 +1,32 @@
+# Script:                       Ops401d7 Ops Challenge 01
+# Author:                       Jose Cardozo
+# Date of latest revision:      02/09/2023
+# Purpose:                      Automate Screen Lock
+
+# Attribution: https://quickbytesstuff.blogspot.com/2014/10/lock-computer-using-powershell.html
+# https://www.techrepublic.com/article/keep-your-windows-sessions-alive-and-ready-with-this-powershell-script/
+
+
+Echo “Computer locking due to inactivity....”
+
+$WShell = New-Object -com “Wscript.Shell”
+
+
+while ($true)
+
+
+{
+
+
+Start-Sleep -Seconds 10
+
+$xCmdString = {rundll32.exe user32.dll,LockWorkStation}
+
+Invoke-Command $xCmdString
+
+
+
+Exit
+}
+
+# End
