598e02a214b183d3a64d30c2019215a077426fe0
jusehcardozo <joseccardozo93@gmail.com>
2023-02-23 16:45:09 -0800

    NOW IT IS DONE!

--- Challenge26.py
+++ Challenge26.py
@@ -6,6 +6,22 @@
 # Purpose:                       Ops Challenge: Event Logging Tool Part 1 of 3
 
 import logging
+import os
+import time
+from datetime import datetime
+import smtplib
+from decouple import config
+
+# I AM APPLYING CHALLENGE 26 INTO CHALLENGE 2!!!
+
+def send_email():
+    today = datetime.today()
+    if current_ping == 0:
+        message1 = "Your host is responding", str(today)
+    else:
+        message1 = "Your host is not responding", str(today)
+    # Sending the mail
+    email.sendmail(username, username, message1)
 
 # Create and configure the logger
 logging.basicConfig(filename="test.log", filemode='w')
@@ -14,10 +30,40 @@ logging.basicConfig(filename="test.log", filemode='w')
 logger = logging.getLogger()
 
 # Set the threshold of logger to DEBUG
-logger.setLevel(logging.Critical)
+logger.setLevel(logging.DEBUG)
 
 # Test messages - These get worse as you go down the list
 logger.debug("Harmless debug message")
 logger.info("Just informantion")
 logger.warning("This is a warning")
 logger.error("This is an error")
+logger.critical("This is critical")
+
+        
+email = smtplib.SMTP('smtp.gmail.com', 587)
+
+email.starttls()
+username = config("username", default='')
+password = config("password", default='')
+email.login(username, password)
+
+current_ping = os.system("ping -c1 127.0.0.1")
+
+while True:
+    last_ping = current_ping
+    logger.debug("System attempting to ping.")
+    current_ping = os.system("ping -c1 127.0.0.1")
+    time.sleep(2)
+    today = datetime.today()
+    print(today)
+
+    if current_ping == 0:
+        logger.info("This is an information that your ping was successful")
+        print(str(today) + " This ping was successful to: 127.0.0.1")
+    else:
+        logger.error("This is an error, the ping did not go through.")
+        print(str(today) +" This ping to: 127.0.0.1 was a failure!")
+    if current_ping != last_ping:
+        send_email()
+        if current_ping != 0:
+            logger.critical("The host is no longer reachable")
