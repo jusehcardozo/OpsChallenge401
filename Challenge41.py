fad49242f92fbb80e6444010dc54e9ae3b4adf41
jusehcardozo <joseccardozo93@gmail.com>
2023-03-13 20:24:30 -0700

    will come back to it

---
+++ Challenge41.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Script:                        Ops 401 Challenge 41
+# Author:                        Jose Cardozo
+# Date of latest revision:       03/13/2023
+# Purpose:                       Ops Challenge: Attack Tools Part 1 of 3
